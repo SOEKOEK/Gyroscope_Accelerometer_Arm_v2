2b97555dab8850f66fba093160a54511e8b92f06
SOEKOEK <benitowildeman@gmail.com>
2019-11-04 01:14:07 +0100

    added error correction

--- Gyroscope_Accelerometer_Arm.ino/Gyroscope_Accelerometer_Arm.ino.ino
+++ Gyroscope_Accelerometer_Arm.ino/Gyroscope_Accelerometer_Arm.ino.ino
@@ -20,10 +20,15 @@
 float elapsedTime, time, timePrev;        //Variables for time control
 float Gyr_rawX, Gyr_rawY, Gyr_rawZ;     //Here we store the raw data read 
 float Gyro_angle_x, Gyro_angle_y;         //Here we store the angle value obtained with Gyro data
+float Gyro_raw_error_x, Gyro_raw_error_y;
 
 //Acc Variables
 float Acc_rawX, Acc_rawY, Acc_rawZ;    //Here we store the raw data read 
 float Acc_angle_x, Acc_angle_y;          //Here we store the angle value obtained with Acc data
+float Acc_angle_error_x, Acc_angle_error_y;
+
+int acc_error=0;   
+int gyro_error=0;                         
 
 float Total_angle_x, Total_angle_y;
 
@@ -48,6 +53,58 @@ void setup() {
 
   Serial.begin(9600);                     //Remember to set this same baud rate to the serial monitor  
   time = millis();                        //Start counting time in milliseconds
+
+  if(acc_error==0)
+  {
+    for(int a=0; a<200; a++)
+    {
+      Wire.beginTransmission(0x68);
+      Wire.write(0x3B);                       //Ask for the 0x3B register- correspond to AcX
+      Wire.endTransmission(false);
+      Wire.requestFrom(0x68,6,true); 
+      
+      Acc_rawX=(Wire.read()<<8|Wire.read())/4096.0 ; //each value needs two registres
+      Acc_rawY=(Wire.read()<<8|Wire.read())/4096.0 ;
+      Acc_rawZ=(Wire.read()<<8|Wire.read())/4096.0 ;
+
+      
+      /*---X---*/
+      Acc_angle_error_x = Acc_angle_error_x + (radToDeg(atan((Acc_rawY)/sqrt(pow((Acc_rawX),2) + pow((Acc_rawZ),2)))));
+      /*---Y---*/
+      Acc_angle_error_y = Acc_angle_error_y + (radToDeg(atan(-1*(Acc_rawX)/sqrt(pow((Acc_rawY),2) + pow((Acc_rawZ),2))))); 
+      
+      if(a==199)
+      {
+        Acc_angle_error_x = Acc_angle_error_x/200;
+        Acc_angle_error_y = Acc_angle_error_y/200;
+        acc_error=1;
+      }
+    }
+  }//end of acc error calculation   
+  if(gyro_error==0)
+  {
+    for(int i=0; i<200; i++)
+    {
+      Wire.beginTransmission(0x68);            //begin, Send the slave adress (in this case 68) 
+      Wire.write(0x43);                        //First adress of the Gyro data
+      Wire.endTransmission(false);
+      Wire.requestFrom(0x68,4,true);           //We ask for just 4 registers 
+         
+      Gyr_rawX=Wire.read()<<8|Wire.read();     //Once again we shif and sum
+      Gyr_rawY=Wire.read()<<8|Wire.read();
+   
+      /*---X---*/
+      Gyro_raw_error_x = Gyro_raw_error_x + (Gyr_rawX/32.8); 
+      /*---Y---*/
+      Gyro_raw_error_y = Gyro_raw_error_y + (Gyr_rawY/32.8);
+      if(i==199)
+      {
+        Gyro_raw_error_x = Gyro_raw_error_x/200;
+        Gyro_raw_error_y = Gyro_raw_error_y/200;
+        gyro_error=1;
+      }
+    }
+  }//end of gyro error calculation 
 }
 
 void loop() {
@@ -67,9 +124,9 @@ void loop() {
     /*Now in order to obtain the gyro data in degrees/seconds we have to divide first
     the raw value by 32.8 because that's the value that the datasheet gives us for a 1000dps range*/
     /*---X---*/
-    Gyr_rawX = (Gyr_rawX/32.8); 
+    Gyr_rawX = (Gyr_rawX/32.8) - Gyro_raw_error_x; 
     /*---Y---*/
-    Gyr_rawY = (Gyr_rawY/32.8);
+    Gyr_rawY = (Gyr_rawY/32.8) - Gyro_raw_error_y;
     
     /*Now we integrate the raw value in degrees per seconds in order to obtain the angle
     * If you multiply degrees/seconds by seconds you obtain degrees */
@@ -100,9 +157,9 @@ void loop() {
  /*Now in order to obtain the Acc angles we use euler formula with acceleration values
  after that we substract the error value found before*/  
  /*---X---*/
- Acc_angle_x = radToDeg(atan((Acc_rawY)/sqrt(pow((Acc_rawX),2) + pow((Acc_rawZ),2))));
+ Acc_angle_x = radToDeg(atan((Acc_rawY)/sqrt(pow((Acc_rawX),2) + pow((Acc_rawZ),2))))-Acc_angle_error_x;
  /*---Y---*/
- Acc_angle_y = radToDeg(atan(-1*(Acc_rawX)/sqrt(pow((Acc_rawY),2) + pow((Acc_rawZ),2))));
+ Acc_angle_y = radToDeg(atan(-1*(Acc_rawX)/sqrt(pow((Acc_rawY),2) + pow((Acc_rawZ),2))))-Acc_angle_error_y;
 
 
  //////////////////////////////////////Total angle and filter/////////////////////////////////////
@@ -113,14 +170,13 @@ void loop() {
    
 
  //float set_height = 1000.00;
- Serial.print("Yº: ");
  Serial.println(Total_angle_y);
  //float delta_height = calculateDeltaHeight(Total_angle_y,300);
  //Serial.print("Height difference: ");
  //Serial.println(delta_height);
  //Serial.print("Waterheight: ");
  //Serial.println(set_height+delta_height);
- Serial.println(" ");
+ //Serial.println(" ");
  //delay(250);
 }
 
